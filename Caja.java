9dede1b6e834e9ef032492b909c461b1da766f09
moonhunter_code <53928868+moonhuntercode@users.noreply.github.com>
2021-04-26 10:05:27 -0400

    Create Caja.java
    añadiendo clase de comportamiento de clase

---
+++ Caja.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CajaPaquete;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Caja {
+    int ancho,alto,profundo;
+    
+    public Caja(){
+        System.out.println("usando constructor");
+        
+    }
+    public Caja(int arg1,int arg2,int arg3){
+        
+    }
+}
