edfd648e5f23ebfdb812c95ce829040a2cd117d2
moonhunter <vdcloayza@gmail.com>
2021-09-14 16:35:37 -0400

    añadiendo varios programas

--- apiList/ListArray.java
+++ apiList/ListArray.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * ListArray
  */
 public class ListArray {
 
     public static void main(String[] args) {
-        
+        ArrayList<Integer>list=new ArrayList<Integer>();
+        for(int i=0;i<6;i++){
+            list.add(i);
+        }
+        System.out.println(list);
+        int x=0;
+        Iterator<Integer>it=list.iterator();
+        while (it.hasNext()) {
+            x+=it.next();
+
+        }
+        System.out.println(x);
     }
 }
---
+++ whileraro/Whileraroo.java
@@ -0,0 +1,13 @@
+/**
+ * Whileraroo
+ */
+public class Whileraroo {
+
+    public static void main(String[] args) {
+        int i=0;int f=1;
+        while (++i<5) {
+            System.out.println(i);
+        }
+        
+    }
+}
