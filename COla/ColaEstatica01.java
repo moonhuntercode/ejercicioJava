9f503f2784798723dc32bc052f19a2a2e5753db4
moonhunter <vdcloayza@gmail.com>
2021-09-20 18:11:25 -0400

    problemas de conexión mysql arreglados

---
+++ COla/ColaEstatica01.java
@@ -0,0 +1,94 @@
+package COla;
+import java.util.Scanner;
+
+public class CColaEstatica 
+{
+	int MAX=10;
+	int Infor[]=new int[MAX];
+	int Ini;
+	int Fin;
+
+	public CColaEstatica()
+	{
+		Ini=0;
+		Fin=-1;
+	}
+
+	public int Leer_Dato()
+	{
+		Scanner tecla=new Scanner(System.in);
+		int dato=tecla.nextInt();
+		return dato;
+	}
+	
+	public void Insertar(int dato)
+	{
+		if(Fin==MAX-1)
+	        System.out.print("COLA LLENA !!");
+	     else
+	      {
+	         Fin++;
+	         Infor[Fin]=dato;
+	      }
+	}
+	
+	public void Eliminar()
+	  {
+	     if(Fin==-1)
+	        System.out.print("COLA VACIA !!"); 
+	     else
+	     {
+	       System.out.print("Elemento Eliminado= "+ Infor[Ini]);
+	       for(int i=0; i<Fin; i++)
+	          Infor[i]=Infor[i+1];
+	        
+	        Fin--;
+	     }
+	  }
+
+	public void Imprimir()
+	{
+		if(Fin==-1)
+			System.out.print("COLA VACIA!!!");
+		else
+		{
+			for(int i=Ini;i<=Fin;i++)
+			  System.out.print(Infor[i]+" ");	
+		}	
+	}
+	
+	public static void main(String[] args) {
+	 int opcion;
+	 CColaEstatica col=new CColaEstatica();
+	 do {
+		 System.out.println();
+		 System.out.println("1. INSERTAR ");
+		 System.out.println("2. ELIMINAR ");
+		 System.out.println("3. IMPRIMIR COLA ");
+		 System.out.println("4. SALIR ");
+		 System.out.println("ELIJA UNA OPCI�N:  ");
+		 opcion=col.Leer_Dato();
+		 switch(opcion)
+		 {
+			 case 1: System.out.print("Digite el dato a insertar: ");
+			 	     int dato=col.Leer_Dato();
+			 	     col.Insertar(dato);
+			 	     break;
+			 case 2: col.Eliminar();
+			 	     break;
+			 case 3: col.Imprimir();
+			 	     break;
+			 case 4: System.exit(0);
+			 default: System.out.print("Eroor, no existe opci�n ");
+			 
+		 }
+	 }while(opcion!=4);
+
+	}
+
+}
+
+
+
+
+
--- mavenvscode/demo/src/main/java/jdbcmysql/App.java
+++ mavenvscode/demo/src/main/java/jdbcmysql/App.java
@@ -1,5 +1,11 @@
 package jdbcmysql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.*;
+
 /**
  * Hello world!
  *
@@ -8,8 +14,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        String url="jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-        
+        String url="jdbc:mysql://localhost:3306/sakilatest?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+            try{ 
+                // Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection conexion =DriverManager.getConnection(url,"root","admin");
+                java.sql.Statement instruccion= conexion.createStatement();
+                String sql="SELECT actor_id,first_name,last_name,last_update FROM actor";
+                ResultSet resultados=instruccion.executeQuery(sql);
+                while (resultados.next()) {
+                    System.out.println("Id Persona:"+resultados.getInt("actor_id"));
+                }
+
+            }
+            // catch(ClassNotFoundException ex){
+            // }
+             catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         System.out.println( "Hello World!" );
         
     }
