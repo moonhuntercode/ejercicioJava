ef445c29b3359471d2bb99e48cbea048a899082a
moonhunter <vdcloayza@gmail.com>
2021-09-20 02:13:53 -0400

    haciendo cadena de conexion

--- mavenvscode/demo/src/main/java/jdbcmysql/App.java
+++ mavenvscode/demo/src/main/java/jdbcmysql/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        String url="jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        
         System.out.println( "Hello World!" );
+        
     }
 }
