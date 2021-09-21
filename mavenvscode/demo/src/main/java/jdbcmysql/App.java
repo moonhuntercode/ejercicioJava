9c27cb6e1aea48562844f4cfa4ba3dfb0bdcdfea
moonhunter <vdcloayza@gmail.com>
2021-09-20 21:42:54 -0400

    haciendo cambios importantes en conexión

--- mavenvscode/demo/src/main/java/jdbcmysql/App.java
+++ mavenvscode/demo/src/main/java/jdbcmysql/App.java
@@ -14,14 +14,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        String url="jdbc:mysql://localhost:3306/sakilatest?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
             try{ 
+                
                 // Class.forName("com.mysql.cj.jdbc.Driver");
-                Connection conexion =DriverManager.getConnection(url,"root","admin");
+                Connection conexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true","root" ,"admin");
                 java.sql.Statement instruccion= conexion.createStatement();
-                String sql="SELECT actor_id,first_name,last_name,last_update FROM actor";
-                ResultSet resultados=instruccion.executeQuery(sql);
-                while (resultados.next()) {
+                ResultSet sql=instruccion.executeQuery("SELECT actor_id,first_name,last_name,last_update FROM actor");
+                // ResultSet resultados=instruccion.executeQuery(sql);
+                while (sql.next()) {
                     System.out.println("Id Persona:"+resultados.getInt("actor_id"));
                 }
 
