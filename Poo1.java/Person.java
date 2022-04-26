dc010075670a847f26fa5adab97e66997d4316dc
moonhunter <vdcloayza@gmail.com>
2022-04-25 23:49:26 -0400

    the package needed .java extension

---
+++ Poo1.java/Person.java
@@ -0,0 +1,12 @@
+package Poo1;
+public class Person {
+String name;
+String lastname;
+
+
+public void showInfo(){
+    System.out.println("Name: "+name);
+    System.out.println("Last Name: "+lastname);
+
+}
+}
---
+++ Poo1.java/Person2.java
@@ -0,0 +1,6 @@
+package Poo1;   
+public class Person2 {
+    public static void main(String[] args) {
+        
+    }
+}
