a02de529378992a74bd215e0de57ccc0a4620a7e
victorcode <vdcloayza@gmail.com>
2022-06-13 01:58:32 -0400

    solution added

--- stringMethods/Methods.java
+++ stringMethods/Methods.java
@@ -1,3 +1,59 @@
+
+public class Methods {
+    public static void main(String[] args) {
+dna();
+
+    }
+
+    public static void comprobaDna(){
+        String dna1="ATGCGATACGCTTGA";
+        String dna2="ATGCGATACGTGA";
+        String dna3="ATTAATATGTACTGA";
+        String dna=dna2;
+        // Find the length:
+int length = dna.length();
+System.out.println("Length: " + length);
+ // Find start codon: 
+int start = dna.indexOf("ATG");
+System.out.println("Start: " + start);
+// Find stop codon:
+ int stop = dna.indexOf("TGA");
+System.out.println("Stop: " + stop);
+        System.out.println(dna.substring(0,3).equals("ATG"));
+        System.out.println(dna.substring(10,13).equals("TGA"));
+        System.out.println("tamaño"+dna.length());
+        }
+
+    public static void dna(){
+        String dna1="ATGCGATACGCTTGA";
+        String dna2="ATGCGATACGTGA";
+        String dna3="ATTAATATGTACTGA";
+           String dna=dna2;
+        // Find the length:
+       int length = dna.length();
+       System.out.println("Length: " + length);
+        // Find start codon: 
+       int start = dna.indexOf("ATG");
+       System.out.println("Start: " + start);
+       // Find stop codon:
+        int stop = dna.indexOf("TGA");
+       System.out.println("Stop: " + stop);
+       System.out.println("dna+start: "+dna.substring(0,3).equals(start));
+
+       if(
+        (start!=-1 && stop!=-1) && 
+       ((stop-start)%3==0)
+       )
+       {
+        // String protein="";
+            System.out.println("tiene multiplos de 3");
+        }
+            else{
+     System.out.println("no protein.");
+            }
+    }
+
+    public static void DevuelveTamaño(){
 // length()
 // In Java, the length() string method returns the length ⁠— total number of characters ⁠— of a String.
 
@@ -7,12 +63,14 @@
 /**
  * Methods
  */
-public class Methods {
-
-    public static void DevuelveTamaño(){
-        String str = "Hello World!";  
-        System.out.println("el tamaño del str es: "+str.length());
+String str = "Hello World!";  
+System.out.println("el tamaño del str es: "+str.length());
+// 12 would be printed because str has 12 characters:
+// H e l l o _ W o r l d !
+// In theory, the length of a String is the same as the Unicode units of the String. For example, escape sequences such as \n count as only one character.
     }
+
+
     public static  void FullName(){
   // Add your first names:
   String firstName = "Ziggy";  
@@ -21,14 +79,16 @@ public class Methods {
   // What's the full name?
   System.out.println(firstName.concat(" " + lastName));
     }
-    public static void main(String[] args) {
-        DevuelveTamaño();
-FullName();
+
+
+    public static void metodoPaIgualar(){
+        // equals()
+    // With objects, such as Strings, we can’t use the primitive equality operator == to check for equality between two strings. To test equality with strings, we use a built-in method called equals().
+    
+    // For example:
+
+
     }
+
 }
 
-// 12 would be printed because str has 12 characters:
-
-// H e l l o _ W o r l d !
-
-// In theory, the length of a String is the same as the Unicode units of the String. For example, escape sequences such as \n count as only one character.
